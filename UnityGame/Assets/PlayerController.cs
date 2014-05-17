4fb306c24837f504ef0341a62ecc00229f82b353
ameshn <ameshn@ameshs-mbp.novuscom.net>
2014-05-17 15:59:16 -0700

    Player prefab

---
+++ UnityGame/Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public int score;
+	public int meter;
+	public int id;
+	public int currentSign;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Rock" + id)) {
+			Debug.Log("Pressed Rock");
+		}
+
+		if (Input.GetButtonDown ("Paper" + id)) {
+			Debug.Log("Pressed Paper");
+		}
+
+		if (Input.GetButtonDown ("Scissors" + id)) {
+			Debug.Log("Pressed Scissors");
+		}
+	}
+
+}
