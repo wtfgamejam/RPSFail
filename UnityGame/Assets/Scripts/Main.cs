16f37db13a3ef96d38f7a656356096ab05b9db57
ameshn <ameshn@ameshs-mbp.novuscom.net>
2014-05-17 21:43:59 -0700

    Main controller

---
+++ UnityGame/Assets/Scripts/Main.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	public int currentSign = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Resolve(int in_newSign)
+	{
+		int test = (currentSign + 1) % 3;
+
+		// is win?
+		if( in_newSign == test )
+		{
+
+		}
+	}
+
+}
