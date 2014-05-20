7e89af53e384c98b7555a35da6e64f207570c712
ameshn <amesh.narsing@gmail.com>
2014-05-19 21:34:49 -0700

    Made a shitty help screen

--- UnityGame/Assets/Scripts/TitleScript.cs
+++ UnityGame/Assets/Scripts/TitleScript.cs
@@ -7,14 +7,19 @@ public class TitleScript : MonoBehaviour {
 	public SpriteRenderer paper;
 	public SpriteRenderer scissors;
 
+	public SpriteRenderer controls;
+
 	private bool player1Ready = false;
 	private bool player2Ready = false;
 	private bool player3Ready = false;
 
 	private bool isReady = false;
+	private bool showControls = false; 
 
 	// Use this for initialization
 	void Start () {
+		controls.enabled = false;
+
 		PlayerPrefs.DeleteAll();
 
 		iTween.MoveTo(GameObject.Find("rock"), iTween.Hash("y",1,
@@ -35,6 +40,34 @@ public class TitleScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetButtonDown ("Select1") ||
+		    Input.GetButtonDown ("Select1") ||
+		    Input.GetButtonDown ("Select1") ||
+		    Input.GetKeyDown(KeyCode.Escape))
+		{
+			showControls = !showControls;
+			if(showControls){
+				GameObject.Find("Player1Text").guiText.enabled = false;
+				GameObject.Find("Player2Text").guiText.enabled = false;
+				GameObject.Find("Player3Text").guiText.enabled = false;
+				GameObject.Find("PressStart1").guiText.enabled = false;
+				GameObject.Find("PressStart2").guiText.enabled = false;
+				GameObject.Find("PressStart3").guiText.enabled = false;
+				controls.enabled = true;
+			}
+			else
+			{
+				GameObject.Find("Player1Text").guiText.enabled = true;
+				GameObject.Find("Player2Text").guiText.enabled = true;
+				GameObject.Find("Player3Text").guiText.enabled = true;
+				GameObject.Find("PressStart1").guiText.enabled = true;
+				GameObject.Find("PressStart2").guiText.enabled = true;
+				GameObject.Find("PressStart3").guiText.enabled = true;
+
+				controls.enabled = false;
+			}
+		}
+
 		if (Input.GetButtonDown ("Start1")) {
 			Debug.Log("Pressed Start 1");
 			rock.color = new Color(.963F, 0.428F, 0.428F, 1F);
